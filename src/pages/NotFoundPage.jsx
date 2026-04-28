91efe89f2fbe3b18e786b5bd548967968d639e7a
Ваше Имя <.email@example.com>
2026-04-28 17:01:16 +0600

    first commit

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./NotFoundPage.css";
-
 const NotFoundPage = () => {
   return (
     <div className="notfound-container">
